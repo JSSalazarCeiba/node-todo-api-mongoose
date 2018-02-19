dbe283765c328b547df769c0712ffe33fd295043
Juan Sebastián Salazar Aguirre - Ceiba Software <juan.salazar@ceiba.com.co>
2018-02-19 17:54:43 -0500

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,51 @@
+// Load modules
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+// Clean database before Test
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
+
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((error) => done(error));
+      });
+  });
+
+  it('should not create a new todo with invalid body data', (done) => {
+    var text = '';
+
+    request(app)
+      .post('/todos')
+      .expect(400)
+      .end((err, res) => {
+        if (err) return done(err);
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((error) => done(error));
+      });
+  });
+});
