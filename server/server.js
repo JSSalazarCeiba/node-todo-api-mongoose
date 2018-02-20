da2d73b63203dc6202f6184d9f24c12d61d79031
Juan Sebastián Salazar Aguirre - Ceiba Software <juan.salazar@ceiba.com.co>
2018-02-20 12:10:06 -0500

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Load modules
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -65,6 +66,28 @@ app.delete('/todos/:id', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
+});
+
+/* --- PATCH ---*/
+// Update a document by id
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then((todo) => {
+      if (!todo) return res.status(404).send();
+      res.send({todo});
+    })
+    .catch((e) => res.status(400).send());
 });
 
 // Export modules
